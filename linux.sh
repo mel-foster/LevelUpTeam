e1e225c8a76b2f0303386bd25e9b2215d5917132
mel-foster <melfreeman83@email.com>
2023-03-05 08:50:50 +0000

    mel foster made changes to this file

--- linux.sh
+++ linux.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 
-echo "Please enter your name:Mel Foster"
-read name
+echo "Please enter your name: Mel Foster"
+read  name
 echo "Reading.... please wait"
 sleep 3 
 echo "-----------------------------"
-echo "Your username is:" && mel-foster 
+echo "Your username is:" && mel-foster  
 echo "-----------------------------"
 echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && 03/04/2023 
+echo "Made on:" && date
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:" && curl ifconfig.me
 echo "-----------------------------"
-sleep 2
-
+sleep 2	
 
 This is week 4 - project test
-#Note Mel made changes to file
+#Edited by Mel
