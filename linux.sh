27269ac7ef62d33e002537f43b0bf1b91807ec71
mel-foster <melfreeman83@email.com>
2023-03-05 09:41:11 +0000

    mel foster updated attempt to change file

--- linux.sh
+++ linux.sh
@@ -11,7 +11,7 @@ echo "This is one of my first Linux Scripts"
 sleep 2
 echo "Please wait while we process the date......."
 sleep 3
-echo "Made on:" && date
+echo "Made on:" && 03/04/2023
 echo "-----------------------------"
 sleep 2
 echo "My IP address is:" && curl ifconfig.me
